334e87fa1cfd1bf323b54dab9ed397f09ad40a78
Watcharapong Phubanchao <76417323+RimonTzz@users.noreply.github.com>
2025-03-20 01:52:40 +0700

    add column in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
 
 
   enum :role, { super_admin: 0, admin: 1, user: 2 }
+  attribute :first_name, :string
+  attribute :last_name, :string
+  attribute :phone_number, :string
 
   has_many :timesheets
   has_many :user_projects
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_19_100253) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_19_184140) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -69,6 +69,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_19_100253) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role", default: 2, null: false
+    t.string "first_name" #อย่าลืมเข้ามาเพิ่ม not null: false
+    t.string "last_name"
+    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
