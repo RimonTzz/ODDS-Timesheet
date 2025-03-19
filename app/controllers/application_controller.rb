4ffb2f461f5fc7088fcff89580fbea1646bfcbb0
Watcharapong Phubanchao <76417323+RimonTzz@users.noreply.github.com>
2025-03-19 14:22:12 +0700

    add permission for role

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,37 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   before_action :authenticate_user!
+  helper_method :current_user_is_admin?
+  helper_method :current_user_is_super_admin?
+
+  def current_user_is_admin?
+    current_user&.admin? || current_user&.super_admin?
+  end
+
+  def current_user_is_super_admin?
+    current_user&.super_admin?
+  end
+
+  def authorize_admin! #access when admin and superadmin
+    return if current_user_is_admin?
+
+    flash[:alert] = "คุณไม่มีสิทธิ์เข้าถึงส่วนนี้"
+    redirect_to root_path
+  end
+
+  def authorize_super_admin! #just super admin allow
+    return if current_user_is_super_admin?
+
+    flash[:alert] = "คุณไม่มีสิทธิ์เข้าถึงส่วนนี้"
+    redirect_to root_path
+  end
+
+  def authorize_not_user!
+    return unless current_user # ป้องกันกรณี current_user เป็น nil
+
+    if current_user.role == "user"
+      flash[:alert] = "คุณไม่มีสิทธิ์เข้าถึงส่วนนี้"
+      redirect_to timesheets_path
+    end
+  end
 end
--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,5 +1,7 @@
 class ClientsController < ApplicationController
   before_action :set_client, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!
+  before_action :authorize_not_user!
 
   # GET /clients or /clients.json
   def index
