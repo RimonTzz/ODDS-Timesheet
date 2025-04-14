69c6d83a17831856541521227e40d7d4395f8b48
Watcharapong Phubanchao <76417323+RimonTzz@users.noreply.github.com>
2025-04-14 21:22:50 +0700

    refactor project users controller and new member form for improved functionality and user experience

--- app/controllers/project_users_controller.rb
+++ app/controllers/project_users_controller.rb
@@ -1,8 +1,6 @@
-
 class ProjectUsersController < ApplicationController
+  before_action :set_project, only: [:index, :new, :create, :edit, :update, :destroy]
   before_action :authenticate_user!
-  before_action :set_project,  only: [ :index, :new, :create, :destroy, :edit, :update ]
-  before_action :set_user_project, only: [:edit, :update, :destroy]
   before_action :authorize_admin!
 
   def index
@@ -11,55 +9,45 @@ class ProjectUsersController < ApplicationController
   end
 
   def new
-    @user_project = @project.user_projects.new
     @available_users = User.where.not(id: @project.users.pluck(:id))
   end
 
   def create
-    user = User.find_by(id: params[:user_id]) # ใช้ find_by เพื่อป้องกัน RecordNotFound
-    @user_project = @project.user_projects.new(user: user, position: params[:user_project][:position])
+    @user_project = UserProject.new(
+      project_id: params[:project_id],
+      user_id: params[:user_id],
+      position: params[:position]
+    )
 
     if @user_project.save
-      redirect_to project_users_path(@project), notice: "เพิ่ม #{user.email} ในตำแหน่ง #{@user_project.position.titleize} เข้าสู่โปรเจ็คแล้ว"
+      redirect_to project_users_path(@project), notice: 'Member was successfully added.'
     else
-      @available_users = User.where.not(id: @project.users.pluck(:id))
-      render :new, status: :unprocessable_entity
+      render :new
     end
   end
 
   def edit
-    Rails.logger.debug "Params in edit action: #{params.inspect}"
+    @user_project = UserProject.find_by(user_id: params[:id], project_id: @project.id)
   end
 
   def update
-    if @user_project.update(user_project_params)
-      redirect_to project_users_path(@project), notice: "#{@user_project.user.email} ได้รับการอัปเดตตำแหน่งแล้ว"
+    @user_project = UserProject.find_by(user_id: params[:id], project_id: @project.id)
+    if @user_project.update(position: params[:position])
+      redirect_to project_users_path(@project), notice: 'Member was successfully updated.'
     else
-      render :edit, status: :unprocessable_entity
+      render :edit
     end
   end
 
   def destroy
-    user = User.find(params[:id])
-    @project.users.delete(user)
-    redirect_to project_users_path(@project), alert: "ลบ #{user.email} ออกจากโปรเจ็คแล้ว"
+    @user_project = UserProject.find_by(user_id: params[:id], project_id: @project.id)
+    @user_project.destroy
+    redirect_to project_users_path(@project), notice: 'Member was successfully removed.'
   end
 
   private
+
   def set_project
     @project = Project.find(params[:project_id])
   end
-
-  def user_project_params
-    params.require(:user_project).permit(:position)
-  end
-
-  def set_user_project
-    @user_project = UserProject.find_by(user_id: params[:id], project: @project)
-    unless @user_project
-      flash[:alert] = "ไม่พบข้อมูลผู้ใช้ในโปรเจ็ค"
-      redirect_to project_users_path(@project)
-    end
-  end
 end
-
