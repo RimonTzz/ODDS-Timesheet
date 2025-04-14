7528bb6361d10dc864f8c0b120457577eb1d421c
Watcharapong Phubanchao <76417323+RimonTzz@users.noreply.github.com>
2025-04-14 20:09:27 +0700

    refactor project creation and update actions for improved flow; add flash messages to index view

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -25,27 +25,20 @@ class ProjectsController < ApplicationController
   def create
     @project = Project.new(project_params)
 
-    respond_to do |format|
-      if @project.save
-        format.html { redirect_to @project, notice: "Project was successfully created." }
-        format.json { render :show, status: :created, location: @project }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
+    if @project.save
+      redirect_to projects_path
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /projects/1 or /projects/1.json
   def update
-    respond_to do |format|
-      if @project.update(project_params)
-        format.html { redirect_to @project, notice: "Project was successfully updated." }
-        format.json { render :show, status: :ok, location: @project }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+      redirect_to projects_path
+    else
+      render :edit
     end
   end
 
