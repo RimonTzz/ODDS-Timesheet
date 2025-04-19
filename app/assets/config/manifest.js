b5d1edb81f350d4f8586d78a418cc4fa1dcea321
Watcharapong Phubanchao <76417323+RimonTzz@users.noreply.github.com>
2025-04-20 00:14:25 +0700

    Update manifest.js to link application.js and additional controllers

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,8 @@
 //= link application.css
+//= link application.js
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link controllers/application.js
+//= link controllers/flash_controller.js
+//= link controllers/hello_controller.js
+//= link controllers/index.js
